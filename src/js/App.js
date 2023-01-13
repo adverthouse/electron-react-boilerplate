a2954f7323a6b81ebbebbb76b806b9b1a331cd35
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2023-01-13 17:33:01 +0300

    .

--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,7 @@ export default function App() {
 
   return (
     <>
-      <h1>I am App Component!!! erwrw</h1>
+      <h1>I am App Component!!! Hello !</h1>
       <button className='btn btn-primary' onClick={() => {
           electron.notificationApi.sendNotification('My custom notification!');
        }}>Notify</button>
