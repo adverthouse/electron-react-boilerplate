c413263d87c917e990edb4bf8d47b72df7f9eaf8
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2023-01-13 17:26:58 +0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-
-const { BrowserWindow, app, ipcMain, Notification } = require('electron');
+const { BrowserWindow, app, Menu,ipcMain  } = require('electron');
 const path = require('path');
+const url = require("url");
 
 const isDev = !app.isPackaged;
+ 
+let mainwindow; 
 
-function createWindow() {
-  const win = new BrowserWindow({
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
+
+app.on("ready", ()=> {
+
+  mainwindow = new BrowserWindow({
     width: 1200,
     height: 800,
     backgroundColor: "white",
@@ -17,17 +26,67 @@ function createWindow() {
     }
   })
 
-  win.loadFile('index.html');
+  mainwindow.loadURL(
+    url.format({
+      pathname : path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes : true
+    })
+  );
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  ipcMain.on('notify', (_, message) => {
+    console.log(message);
+    //new Notification({title: 'Notifiation', body: message}).show();
+  });
+
+});
+
+
+const mainMenuTemplate = [
+  {
+     label: "Dosya",
+     submenu : [
+        {
+            label : "Ekle"
+        },
+        {
+          label : "Tümünü sil"
+        }, 
+        {
+          label : "Çıkış",
+          accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+          role :"quit"
+        }
+     ]
+  }
+]
+
+if (process.platform == "darwin"){
+   mainMenuTemplate.unshift({
+      label : app.getName(),
+      role: "TODO"
+   });
 }
 
-if (isDev) {
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-  })
+if (process.env.NODE_ENV != "production"){
+   mainMenuTemplate.push( {
+    label: "Dev Tools",
+    submenu : [
+      { 
+        label : "Dev",
+        click(item, focusedWindow){
+            focusedWindow.toggleDevTools();
+        }
+      },
+      { 
+        label : "Reload",
+        click(item, focusedWindow){
+            focusedWindow.reload();
+        }
+      }
+    ]
+  });
 }
-
-ipcMain.on('notify', (_, message) => {
-  new Notification({title: 'Notifiation', body: message}).show();
-})
-
-app.whenReady().then(createWindow)
--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,14 @@
 
 import React from 'react';
 
-
 export default function App() {
 
   return (
     <>
-      <h1>I am App Component!!!</h1>
-      <button onClick={() => {
-        electron.notificationApi.sendNotification('My custom notification!');
-      }}>Notify</button>
+      <h1>I am App Component!!! erwrw</h1>
+      <button className='btn btn-primary' onClick={() => {
+          electron.notificationApi.sendNotification('My custom notification!');
+       }}>Notify</button>
     </>
   )
 }
